206f1b7686489a8ec4dfb33938f5070b90ecad0b
Rettend <36823200+Rettend@users.noreply.github.com>
2018-09-21 16:48:46 +0200

    Create ics.py

---
+++ ics.py
@@ -0,0 +1,95 @@
+import discord, logging, json, asyncio, time, random, aiohttp, re, datetime, traceback, os, sys, math, asyncpg
+from time import gmtime
+from discord.ext import commands
+from functions import edit_json,read_json
+#-------------------DATA---------------------
+
+version = "0.9.0"
+owner = ["361534796830081024"]
+bot = commands.Bot(command_prefix='-', description=None)
+bot.remove_command("help")
+startup_extensions = ["YouTube"]
+message = discord.Message
+server = discord.Server
+member = discord.Member
+user = discord.User
+Imox = ["365173881952272384"]
+permissions = discord.Permissions
+PRserver = "PissRocket"
+underworking = ":warning: **Meh Boi, this command hasn't finished. Please wait until it's got.** :warning:"
+"""timer = time.strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())"""
+#--------------------------------------------
+
+#-----------------SETUP----------------------
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    await bot.change_presence(game=discord.Game(name=''))
+
+class NoPermError(Exception):
+    pass
+
+#--------------------------------------------
+
+#----------------COMMANDS--------------------      
+@bot.command(pass_context=True)
+async def ping(ctx):
+    before = time.monotonic()
+    embed = discord.Embed(description=":ping_pong: **...**", colour=0x3498db)
+    msg = await bot.say(embed=embed)
+    ping = (time.monotonic() - before) * 1000
+    pinges = int(ping)
+    if 999 > pinges > 400:
+        mesg = "Thats a lot!"
+    elif pinges > 1000:
+        mesg = "Omg, really sloooooow...."
+    elif 399 > pinges > 141:
+        mesg = "Ahhh, not good!"
+    elif pinges < 140:
+        mesg = "Its Good, Boi ;)"
+    em = discord.Embed(title=None, description=f":ping_pong: Seems like `{pinges}` MS\n{mesg}", colour=0x3498db)
+    em.set_author(name=ctx.message.author, icon_url=ctx.message.author.avatar_url)
+    timer = time.strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+    em.set_footer(text=timer)
+    await bot.edit_message(msg, embed=em)
+
+@bot.command(pass_context=True)
+async def nick(ctx, *, name=None):
+    if name is None:
+        await bot.reply("**The usage is `r-name {Something to set as your name}` ty.**")
+    else:
+        await bot.change_nickname(ctx.message.author, name)
+        em = discord.Embed(title="Nickname", description=f"{ctx.message.author}'s nick set to __{name}__!", colour=0x3498db)
+        await bot.say(embed=em)
+
+@bot.command(pass_context=True)
+async def say(ctx, *, words=None):
+    if words is None:
+        await bot.reply("**The usage is `r-say {Something}` ty.**")
+    else:
+        await bot.say(f"**{words}**")
+#-----------------------------------------------
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith("-time"):
+        timer = time.strftime("%a, %d %b %Y %H:%M:%S +0000", gmtime())
+        await bot.send_message(message.channel, f"**{message.author.mention}, the time is __{timer}__**")
+    if message.content.startswith("-help"):
+        Rettend = discord.utils.get(message.server.members, id="361534796830081024")
+        em = discord.Embed(title="HELP", description="do `-list`", colour=0x3498db)
+        await bot.send_message(message.channel, embed=em)
+        await bot.send_message(message.channel, embed=em)
+    if message.content.startswith('-list 1'):
+        emb = discord.Embed(title='MY COMMANDS:', description="Hey, check out my commands!", colour=0x3498db)
+        emb.add_field(name='--------------------', value='', inline=False)
+        emb.set_thumbnail(url='https://cdn.discordapp.com/emojis/385152309090451467.png?v=1')
+        await bot.send_message(message.channel, embed=emb)
+    await bot.process_commands(message) #IMPORTANT
+
+
+token = os.environ.get('DISCORD_TOKEN')
+bot.run(token)
